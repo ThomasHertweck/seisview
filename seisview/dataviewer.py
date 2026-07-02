a32081ad67906cdb6155989d5e3f87ead0ffd27d
Thomas Hertweck <Thomas.Hertweck@kit.edu>
2026-07-02 11:02:50 +0200

    Restructured files.

--- seisview/dataviewer.py
+++ seisview/dataviewer.py
@@ -19,7 +19,6 @@ import tkinter as tk
 
 from matplotlib import rcParams
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from matplotlib.colors import LinearSegmentedColormap
 from matplotlib.ticker import MultipleLocator, AutoLocator, MaxNLocator
 from tkinter import ttk
 # from ttkbootstrap_icons_fa import FAIcon
@@ -27,154 +26,8 @@ from ttkbootstrap_icons_bs import BootstrapIcon
 from tktooltip import ToolTip
 
 from . import __version__, __author__
+from . import colormaps as cm
 
-###############################################################################
-#ColorMaps
-###############################################################################
-
-class ColorMaps:
-    @classmethod
-    def seismic_blwr(cls, version=1):
-        """
-        Seismic black-white-red colormap.
-
-        Version 1 equals Matplotlib's standard 'RdGy', while version 2 has a
-        brighter 'red' component.
-        """
-        if version == 1:
-            return plt.get_cmap(name="RdGy")
-        else:
-            _seismic_blwr_colors = [[1, 0, 0], [1, 1, 1], [0, 0, 0]]
-            _seismic_blwr_nodes = [0, 0.5019607843137255, 1.0]
-            return LinearSegmentedColormap.from_list("seismic_blwr", list(zip(_seismic_blwr_nodes, _seismic_blwr_colors)))
-
-    @classmethod
-    def seismic_blwr_r(cls, version=1):
-        """
-        Reversed seismic black-white-red colormap.
-
-        Version 1 equals Matplotlib's standard 'RdGy', while version 2 has a
-        brighter 'red' component.
-        """
-        return cls.seismic_blwr(version=version).reversed()
-
-    @classmethod
-    def seismic_bwr(cls):
-        """Seismic blue-white-red colormap."""
-        _seismic_bwr_colors = [[0, 0, 1], [1, 1, 1], [1, 0, 0]]
-        _seismic_bwr_nodes = [0, 0.5019607843137255, 1.0]
-        return LinearSegmentedColormap.from_list("seismic_bwr", list(zip(_seismic_bwr_nodes, _seismic_bwr_colors)))
-
-    @classmethod
-    def seismic_bwr_r(cls):
-        """Reversed seismic blue-white-red colormap."""
-        return cls.seismic_bwr().reversed()
-
-    @classmethod
-    def bwr(cls, alpha=0.5):
-        """
-        Blue-white-red colormap with optional transparency in the center.
-
-        alpha is the transparency value between 0 and 1.
-        """
-        bwr_dict = {'red': ((0, 0, 0), (0.25, 0, 0), (0.5, 1, 1), (0.75, 0.8314, 0.8314), (1, 0.5, 0.5)),
-                    'green': ((0, 0, 0), (0.25, 0.375, 0.375), (0.5, 1, 1), (0.75, 0.375, 0.375), (1, 0, 0)),
-                    'blue': ((0, 0.5, 0.5), (0.25, 0.8314, 0.8314), (0.5, 1, 1), (0.75, 0, 0), (1, 0, 0)),
-                    'alpha': ((0, 1, 1), (0.5, alpha, alpha), (1, 1, 1)),}
-        return LinearSegmentedColormap("bwr", bwr_dict)
-
-    @classmethod
-    def bwr_r(cls, alpha=0.5):
-        """
-        Reversed blue-white-red colormap with optional transparency in the center.
-
-        alpha is the transparency value between 0 and 1.
-        """
-        return cls.bwr(alpha=alpha).reversed()
-
-    @classmethod
-    def opendtect(cls):
-        """Seismic colormap similar to opendtect's default seismic colormap."""
-        _opendtect_colors = [[0.0078431372549020, 0.0078431372549020, 0.01568627450980392],
-                             [0.1960784313725490, 0.2823529411764706, 0.50588235294117640],
-                             [0.9529411764705882, 0.9490196078431372, 0.93725490196078430],
-                             [1.0000000000000000, 0.7764705882352941, 0.00000000000000000],
-                             [0.9921568627450981, 0.1058823529411765, 0.00000000000000000],
-                             [0.6823529411764706, 0.0078431372549020, 0.00000000000000000]]
-        _opendtect_nodes = [0, 0.13725490196078433, 0.5019607843137255, 0.7764705882352941, 0.9372549019607843, 1.0]
-        return LinearSegmentedColormap.from_list("opendtect", list(zip(_opendtect_nodes, _opendtect_colors)))
-
-    @classmethod
-    def opendtect_r(cls):
-        """Reversed seismic colormap similar to opendtect's default seismic colormap."""
-        return cls.opendtect().reversed()
-
-    @classmethod
-    def seismic_clip(cls):
-        """Seismic colormap with clipped extreme values."""
-        _seismic_clip_colors = [[0.2235294117647059, 0.803921568627451, 0.8509803921568627],
-                                [0.0000000000000000, 0.000000000000000, 1.0000000000000000],
-                                [1.0000000000000000, 1.000000000000000, 1.0000000000000000],
-                                [1.0000000000000000, 0.000000000000000, 0.0000000000000000],
-                                [0.9333333333333333, 0.800000000000000, 0.2313725490196079]]
-        _seismic_clip_nodes = [0, 0.00392156862745098, 0.5019607843137255, 0.996078431372549, 1.0]
-        return LinearSegmentedColormap.from_list("seismic_clip", list(zip(_seismic_clip_nodes, _seismic_clip_colors)))
-
-    @classmethod
-    def seismic_clip_r(cls):
-        """Reversed seismic colormap with clipped extreme values."""
-        return cls.seismic_clip().reversed()
-
-    @classmethod
-    def banded(cls):
-        """Banded colormap."""
-        _banded_colors = [[0.5372549019607843, 0.0, 0.7764705882352941],
-                          [0.0, 0.0, 0.45098039215686275],
-                          [0.0, 0.0, 1.0],
-                          [0.0, 0.1607843137254902, 0.5333333333333333],
-                          [0.0, 0.5803921568627451, 1.0],
-                          [0.0, 0.3137254901960784, 0.37254901960784315],
-                          [0.0, 1.0, 1.0],
-                          [0.0, 0.3843137254901961, 0.2549019607843137],
-                          [0.5019607843137255, 1.0, 0.5019607843137255],
-                          [0.3411764705882353, 0.4666666666666667, 0.0],
-                          [1.0, 1.0, 0.0],
-                          [0.47058823529411764, 0.34901960784313724, 0.0],
-                          [1.0, 0.5098039215686274, 0.0],
-                          [0.48627450980392156, 0.13725490196078433, 0.0],
-                          [1.0, 0.0, 0.0],
-                          [0.5058823529411764, 0.0, 0.0],
-                          [0.7294117647058823, 0.0, 0.0],
-                          [0.2901960784313726, 0.0, 0.0],
-                          [0.5019607843137255, 0.0, 0.0]]
-        _banded_nodes = [0.0, 0.054901960784313725, 0.10980392156862745,
-                         0.16862745098039217, 0.2235294117647059, 0.2784313725490196,
-                         0.3333333333333333, 0.38823529411764707, 0.44313725490196076,
-                         0.5019607843137255, 0.5568627450980392, 0.611764705882353,
-                         0.6666666666666666, 0.7215686274509804, 0.7803921568627451,
-                         0.8352941176470589, 0.8901960784313725, 0.9450980392156862, 1.0]
-        return LinearSegmentedColormap.from_list("banded", list(zip(_banded_nodes, _banded_colors)))
-
-    @classmethod
-    def banded_r(cls):
-        """Reversed banded colormap."""
-        return cls.banded().reversed()
-
-    @classmethod
-    def spectrum(cls):
-        """Spectrum colormap."""
-        _spectrum_colors = [[0.0, 0.5568627450980392, 1.0],
-                            [0.0, 1.0, 1.0],
-                            [1.0, 1.0, 0.0],
-                            [1.0, 0.0, 0.0],
-                            [0.5019607843137255, 0.0, 0.0]]
-        _spectrum_nodes = [0.0, 0.24313725490196078, 0.6274509803921569, 0.8784313725490196, 1.0]
-        return LinearSegmentedColormap.from_list("spectrum", list(zip(_spectrum_nodes, _spectrum_colors)))
-
-    @classmethod
-    def spectrum_r(cls):
-        """Reversed spectrum colormap."""
-        return cls.spectrum().reversed()
 
 ###############################################################################
 # DataViewer
@@ -278,10 +131,10 @@ class DataViewer:
         self.cmap_options = ["seismic", "Greys", "black-white-red", "red-white-black", "blue-white-red",
                              "red-white-blue", "opendtect", "opendtect-reversed", "clip", "clip-reversed",
                              "banded", "banded-reversed", "spectrum", "spectrum-reversed"]
-        self.cmap_calls = ["seismic", "Greys", ColorMaps.seismic_blwr(version=2), ColorMaps.seismic_blwr_r(version=2),
-                     ColorMaps.seismic_bwr(), ColorMaps.seismic_bwr_r(), ColorMaps.opendtect(), ColorMaps.opendtect_r(),
-                     ColorMaps.seismic_clip(), ColorMaps.seismic_clip_r(), ColorMaps.banded(), ColorMaps.banded_r(),
-                     ColorMaps.spectrum(), ColorMaps.spectrum_r()]
+        self.cmap_calls = ["seismic", "Greys", cm.seismic_blwr(version=2), cm.seismic_blwr_r(version=2),
+                     cm.seismic_bwr(), cm.seismic_bwr_r(), cm.opendtect(), cm.opendtect_r(),
+                     cm.seismic_clip(), cm.seismic_clip_r(), cm.banded(), cm.banded_r(),
+                     cm.spectrum(), cm.spectrum_r()]
         # status updates
         self.red_style = ttk.Style()
         self.red_style.configure("RedText.TLabel", foreground="red")
@@ -299,7 +152,7 @@ class DataViewer:
         if not self.imshow:
             return ""
         nt, ns = self.ensemble["data"].shape
-        if nt == 1:
+        if self.h[-1] == self.h[0]:
             col_idx = 0
         else:
             # map physical X to column index
@@ -1582,7 +1435,7 @@ def main():
     #     # macOS handles this natively. For Linux, Tkinter provides a built-in scaling method.
 
     DESC = """description: interactive seismic data viewer"""
-    PROG = "seisiview"
+    PROG = "seisview"
 
     parser = argparse.ArgumentParser(prog=PROG, description=DESC, add_help=True)
     parser.add_argument("-v", "--verbose",
@@ -1606,3 +1459,7 @@ def main():
     root = tk.Tk()
     app = DataViewer(root)
     root.mainloop()
+
+# in case somebody calls "python -m seisview.dataviewer" directly.
+if __name__ == "__main__":
+    main()
